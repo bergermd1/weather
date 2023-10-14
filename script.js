8604ba1b485b2afb4c7623d87ee9b964c41cad27
Matt Berger <bergermd1@gmail.com>
2023-10-14 13:38:18 -0500

    Add some html processing.

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-window.onload = () => {
-    processData('Lima', 3)
-}
+let weatherData = '';
 
+window.onload = () => {
+    // const weatherData = processData('Lima', 3);
+    processData('Lima', 3).then((processedData) => {
+            weatherData = processedData;
+            console.log(weatherData);
+            document.querySelector('body').appendChild(getHeader(weatherData.locationData));
+            document.querySelector('body').appendChild(processHTML(weatherData));
+        }
+    );
+}
 
 async function processData(query, days) {
     const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c6ef69ffc86c43afb2b232327231310&q=${query}&days=${days}`, {mode: 'cors'});
     const weatherData = {};
+    const locationData = {};
+    const dailyData = [];
     const hourlyData = [];
     
     const json = await response.json();
     console.log(json);
 
+    locationData.country = json.location.country;
+    locationData.region = json.location.region;
+
     json.forecast.forecastday.forEach((day, dayIndex) => {
+        dailyData.push({
+            conditionText: json.forecast.forecastday[dayIndex].day.condition.text,
+            conditionIcon: processIconPath(json.forecast.forecastday[dayIndex].day.condition.icon),
+            minTempF: json.forecast.forecastday[dayIndex].day.mintemp_f,
+            minTempC: json.forecast.forecastday[dayIndex].day.mintemp_c,
+            maxTempF: json.forecast.forecastday[dayIndex].day.maxtemp_f,
+            maxTempC: json.forecast.forecastday[dayIndex].day.maxtemp_c,
+        })
+        
         day.hour.forEach((hour, hourIndex) => {
-            // console.log(json.forecast.forecastday[dayIndex]);
             hourlyData.push({
-                    time: json.forecast.forecastday[dayIndex].hour[hourIndex].time,
-                    tempC: json.forecast.forecastday[dayIndex].hour[hourIndex].temp_c,
-                    tempF: json.forecast.forecastday[dayIndex].hour[hourIndex].temp_f,
-                    isDay: json.forecast.forecastday[dayIndex].hour[hourIndex].is_day,
-                    chanceOfRain: json.forecast.forecastday[dayIndex].hour[hourIndex].chance_of_rain,
-                });
+                time: json.forecast.forecastday[dayIndex].hour[hourIndex].time,
+                tempC: json.forecast.forecastday[dayIndex].hour[hourIndex].temp_c,
+                tempF: json.forecast.forecastday[dayIndex].hour[hourIndex].temp_f,
+                isDay: json.forecast.forecastday[dayIndex].hour[hourIndex].is_day,
+                chanceOfRain: json.forecast.forecastday[dayIndex].hour[hourIndex].chance_of_rain,
+            });
         });
     });
 
-    console.log(hourlyData);
+    weatherData.locationData = locationData;
+    weatherData.dailyData = dailyData;
+    weatherData.hourlyData = hourlyData;
 
-    // hourlyData
-
-    // weatherData.max = json.forecast.forecastday[0].;
-    // weatherData.condtionIcon = processIconPath(json.current.condition.icon);
-    // weatherData.tempC = json.current.temp_c;
-    // weatherData.tempF = json.current.temp_f;
-    // weatherData.condition = json.current.condition.text;
-    // weatherData.condtionIcon = processIconPath(json.current.condition.icon);
-    // weatherData.tempC = json.current.temp_c;
-    // weatherData.tempF = json.current.temp_f;
-    
-    // console.log(json);
-    return hourlyData;
+    return weatherData;
 }
 
 function processIconPath(str) {
@@ -47,8 +58,29 @@ function processIconPath(str) {
     return 'images/' + str;
 }
 
-// processData('Lima', 3).then(data => {
-//     // console.log(data);
-// })
+function getHeader(locationData) {
+    const header = document.createElement('div');
+    header.classList.add('header');
+    header.textContent = `Local weather for ${locationData.region}, ${locationData.country}`
+    return header;
+}
 
-// http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=London
+function processHTML(data) {
+    // data.dailyData;
+    // data.hourlyData;
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    data.dailyData.forEach((day, dayIndex) => {
+        const dayCard = document.createElement('div');
+        dayCard.classList.add('day-card');
+        dayCard.textContent = day.conditionText;
+        /////add hour elements here
+        data.hourlyData
+        /////
+        container.appendChild(dayCard);
+    });
+
+    return container;
+}
